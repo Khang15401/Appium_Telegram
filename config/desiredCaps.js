856afd713a4f25afd556adfa7d0034d37071eb19
Khang <akhang147@gmail.com>
2024-11-29 15:52:53 +0700

    fix enter amount invest & create file wallet confirm

--- config/desiredCaps.js
+++ config/desiredCaps.js
@@ -9,12 +9,12 @@ async function main() {
         port: 4723,
         path: "/",
         capabilities: {
-            platformName: "Android",
+            "appium:platformName": "Android",
             "appium:platformVersion": "15",
             "appium:deviceName": "emulator-5554",
             "appium:app": "C:\\Users\\akhan\\Downloads\\Telegram.apk",
             "appium:appPackage": "org.telegram.messenger.web",
-            "appium:appActivity": "org.telegram.ui.LaunchActivity",
+            "appium:appActivity": "org.telegram.messenger.DefaultIcon",
             "appium:automationName": "UiAutomator2",
             "appium:noReset": true,
             "appium:fullContextList": true,
@@ -25,9 +25,8 @@ async function main() {
 
     await driver.startActivity(
         "org.telegram.messenger.web",
-        "org.telegram.ui.LaunchActivity",
-        "org.telegram.messenger.web",
-        "org.telegram.ui.LaunchActivity"
+        "org.telegram.messenger.DefaultIcon"
+
     );
 
 
@@ -52,78 +51,89 @@ async function main() {
 
     await driver.pause(12000)
 
-    // const investPopup = await driver.$('android.widget.TextView[text="Let\'s get more package"]');
-    // await investPopup.waitForExist({ timeout: 10000 });
-    // await investPopup.click();
+    //button Login Vercel [745,2134][832,2221]
+    const loginVercelButton = await driver.$('//*[@bounds="[745,2134][832,2221]"]');
+    await loginVercelButton.waitForExist({ timeout: 10000 });
+    await loginVercelButton.click();
 
-    const investButton = await driver.$('//*[@bounds="[126,1693][685,1743]"]');
+    //button disable [259,1764][952,1861]
+    const disableButton = await driver.$('//*[@bounds="[259,1764][952,1861]"]');
+    await disableButton.waitForExist({ timeout: 10000 });
+    await disableButton.click();
+    await driver.pause(2000)
+
+    const investButton = await driver.$('//*[@bounds="[42,1711][1042,1843]"]');
     await investButton.waitForExist({ timeout: 10000 });
     await investButton.click();
 
-    await driver.pause(5000);
-    // const threeMonthOption = await driver.$('//android.view.View[@text="3M" and @clickable="true"]');
-    // await threeMonthOption.waitForExist({ timeout: 6000 });
-    // await threeMonthOption.click();
 
-    // const inputAmount = await driver.$('//android.widget.EditText[@bounds="[42,1617][1042,1735]"]');
-    // await inputAmount.waitForExist({ timeout: 10000 });
-    // await inputAmount.tap();
-    // await inputAmount.sendKeys("0.01");
+    await driver.pause(5000)
 
     const inputBounds = [42, 1617, 1042, 1735];
     const tapX = (inputBounds[2] + inputBounds[0]) / 2;
     const tapY = (inputBounds[3] + inputBounds[1]) / 2;
 
-    await driver.executeScript('mobile: shell', [{
-        command: `input tap ${tapX} ${tapY}`
+    await driver.performActions([{
+        type: 'pointer',
+        id: 'finger1',
+        parameters: { pointerType: 'touch' },
+        actions: [
+            { type: 'pointerMove', duration: 0, x: tapX, y: tapY },
+            { type: 'pointerDown', button: 0 },
+            { type: 'pause', duration: 100 },
+            { type: 'pointerUp', button: 0 }
+        ]
     }]);
+
+    await driver.pause(1000);
 
     await driver.executeScript('mobile: shell', [{
         command: 'input text "0.01"'
     }]);
 
+    await driver.pause(2000);
 
-    const bounds = [42, 2094, 1042,2215];
-    const x = (bounds[2] + bounds[0]) / 2 + 400; 
-    const y = (bounds[3] + bounds[1]) / 2;
 
-    try {
-        await driver.performActions([{
-            type: 'pointer',
-            id: 'finger1', 
-            parameters: { pointerType: 'touch' },
-            actions: [
-                { type: 'pointerMove', duration: 0, x: x, y: y },
-                { type: 'pointerDown', button: 0 },
-                { type: 'pause', duration: 100 },
-                { type: 'pointerUp', button: 0 }
-            ]
-        }]);
+    async function performTapAction(xOffset, yOffset, fingerId, customBounds = [42, 2094, 1042, 2215]) {
+        const x = (customBounds[2] + customBounds[0]) / 2 + xOffset;
+        const y = (customBounds[3] + customBounds[1]) / 2 + yOffset;
 
-    } catch (error) {
-        console.error('Tap is failed:', error.message);
+        try {
+            await driver.performActions([{
+                type: 'pointer',
+                id: fingerId,
+                parameters: { pointerType: 'touch' },
+                actions: [
+                    { type: 'pointerMove', duration: 0, x, y },
+                    { type: 'pointerDown', button: 0 },
+                    { type: 'pause', duration: 100 },
+                    { type: 'pointerUp', button: 0 }
+                ]
+            }]);
+        } catch (error) {
+            console.error('Tap is failed:', error.message);
+        }
     }
 
-    const x1 = (bounds[2] + bounds[0]) / 2 + 400; 
-    const y1 = (bounds[3] + bounds[1]) / 2 + 50;
-    try {
-        await driver.performActions([{
-            type: 'pointer',
-            id: 'finger2', 
-            parameters: { pointerType: 'touch' },
-            actions: [
-                { type: 'pointerMove', duration: 0, x: x1, y: y1 },
-                { type: 'pointerDown', button: 0 },
-                { type: 'pause', duration: 100 },
-                { type: 'pointerUp', button: 0 }
-            ]
-        }]);
+    await performTapAction(400, 0, 'finger1');
+    await driver.pause(2000)
 
-    } catch (error) {
-        console.error('Tap is failed:', error.message);
-    }
+    await performTapAction(400, 50, 'finger2');
+
+    await driver.pause(25000)
+
+    const customBounds = [42, 2107, 1042, 2215];
+    await performTapAction(400, 50, 'finger3', customBounds);
+    await driver.pause(5000);
+
+
+
+
+    const activityPage = await driver.$('//android.view.View[@content-desc="Activity"]')
+    await activityPage.waitForExist({ timeout: 10000 });
+    await activityPage.click();
+
 
 }
 
 main().catch(console.error);
-//adb -s emulator-5554 install C:/Users/akhan/Downloads/Telegram.apk 
